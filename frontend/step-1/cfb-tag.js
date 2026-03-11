104a5f731329ffa35aca942660ad05d6f0880d15
BadWinniePooh <49596084+BadWinniePooh@users.noreply.github.com>
2026-03-11 08:04:17 +0100

    feat: add poc-hello world implementation

--- frontend/step-1/cfb-tag.js
+++ frontend/step-1/cfb-tag.js
@@ -1,5 +1,12 @@
 class CfbTag extends HTMLElement {
-    
+    connectedCallback() {
+        const span = document.createElement('span');
+        span.textContent = "Hello World";
+        span.classList.add('cfb-tag');
+        span.classList.add(`cfb-tag--blue`);
+        
+        this.replaceChildren(span);
+    }
 }
 
 customElements.define('cfb-tag', CfbTag);
