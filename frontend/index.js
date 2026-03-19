164c987f6e8ed528db011e78f8debd414f21137c
BadWinniePooh <49596084+BadWinniePooh@users.noreply.github.com>
2026-03-19 07:33:21 +0100

    cleanup

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,3 @@
 // Update this to use all the components created after each step.
 // This way the main 'index.html' can be modified at each step
-import './cfb-tag.js'
+import './step-1/cfb-tag.js';
